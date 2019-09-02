e08b03dc1187eef17ed7811c16f3c71e55e8112c
Tangjacson <1713653@mail.nankai.edu.cn>
2019-09-02 00:16:42 -0700

    lab01

---
+++ lab/lab01/lab01_extra.py
@@ -0,0 +1,46 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    a=1;
+    while k>0:
+        a*=n-k+1
+        k-=1
+    return a
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    from operator import mod,floordiv
+    while n>10:
+        if(mod(n,100)==88):
+            return True
+        n=floordiv(n,10)
+        continue
+    return False
