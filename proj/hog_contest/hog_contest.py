9d74be73e4d5f948d26fb15c870a59b64b3d7b79
Tangjacson <1713653@mail.nankai.edu.cn>
2019-09-14 16:22:13 -0700

    hc

---
+++ proj/hog_contest/hog_contest.py
@@ -0,0 +1,13 @@
+"""
+Only this file will be submitted. Make sure to include any helper functions
+from `hog.py` that you'll need here! For example, if you have a function to
+calculate Free Bacon points, you should make sure it's added to this file
+as well.
+
+Don't forget: your strategy must be deterministic and pure.
+"""
+
+PLAYER_NAME = '' # Change this line!
+
+def final_strategy(score, opponent_score):
+    return 5
